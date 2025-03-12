acb8c5d9a442268d9406d4385cbba75f6756566b
CPLibrary <villanovaecon@gmail.com>
2025-03-12 13:33:59 -0700

    update

--- template_algos/string_algos/manacher.py
+++ template_algos/string_algos/manacher.py
@@ -11,3 +11,18 @@ def manacher(s):
         if i + Z[i] > right:
             center, right = i, i + Z[i]
     return Z
+
+
+
+def longest_palindrome_from_manacher(s):
+    # Compute the manacher array for s
+    Z = manacher(s)
+    max_len = 0
+    center_index = 0
+    for i, radius in enumerate(Z):
+        if radius > max_len:
+            max_len = radius
+            center_index = i
+    # Map the center in the modified string back to the original string.
+    start = (center_index - max_len) // 2
+    return s[start:start + max_len]
